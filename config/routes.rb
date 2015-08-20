a984e7d6b71a356422362349e758895544f9c4cb
shocyu <sanokebab@gmail.com>
2015-08-20 12:57:10 +0000

    Finish user index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 SampleApp::Application.routes.draw do
   
-  get "users/show"
-  
   devise_for :users, :controllers => {
     :registrations => "registrations"
   }
-  resources :users, only: [:show]
-  
+  resources :users, only: [:show, :index]
   root  'static_pages#home'
+  
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
